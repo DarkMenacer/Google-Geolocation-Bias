e5d3c9898db696dda57758191e237971760833c6
Pranav Chatur <pranavchatur@gmail.com>
2022-09-12 11:52:14 +0530

    Add rbo and scraper_structure

--- scraper_structure.py
+++ scraper_structure.py
@@ -7,20 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 import query_list
 import city_list
 import consts_fxns
-import psycopg2
+from collections import defaultdict
+
+di = defaultdict(list)
 
 PATH = Service("/usr/local/bin/geckodriver")
 options = Options()
 options.add_argument("--incognito")
 driver = webdriver.Firefox(service=PATH, options=options)
 driver.maximize_window()
-conn = psycopg2.connect(dbname=consts_fxns.DB_NAME, user=consts_fxns.DB_USER, password=consts_fxns.DB_PASS, host=consts_fxns.DB_HOST)
-cur = conn.cursor()
 
 try:
-    cur.execute("CREATE TABLE IF NOT EXISTS scraped_links (id SERIAL PRIMARY KEY, city VARCHAR(50),query TEXT, link TEXT);")
-    conn.commit()
     for query in query_list.queries:
+        links_array = list()
         for city in city_list.cities:
             encoded_city = consts_fxns.convert_b64(city)
             key = consts_fxns.secret_keys[len(city)] 
@@ -30,9 +29,11 @@ try:
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR,'.yuRUbf > a'))
             )
             for link in links:
-                cur.execute("INSERT INTO scraped_links(city, query, link) VALUES (%s,%s,%s)",(city,query,link.get_attribute("href")))
-                conn.commit()
+                links_array.append((city, link.get_attribute("href")))
+        di[query] = links_array
+    for query in di:
+        print(query)
+        for i in range(len(di[query])):
+            print(di[query][i][0], di[query][i][1])
 finally:
-    cur.close()
-    conn.close()
     driver.quit()
